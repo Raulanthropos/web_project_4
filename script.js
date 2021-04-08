e34cf05de84eecadffbaa6765115ec90443b97ce
Raul <ipsichias@gmail.com>
2021-04-08 11:41:42 +0300

    feat: modal window works properly - last update before submission for review

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-let popup = document.querySelector('.popup');
-let image = document.querySelector('.profile__container-icon');
-let modal_x = document.querySelector('.modal__form-x');
+let popup = document.querySelector(".popup");
+let image = document.querySelector(".profile__container-icon");
+let modal_x = document.querySelector(".modal__form-x");
 let inputs = document.querySelectorAll(`.modal__form-element`);
+let title = document.querySelector(`.profile__title`);
+let aboutMe = document.querySelector(`.profile__subtitle`);
+let nameInput = document.querySelector(`.modal__form-title`);
+let jobInput = document.querySelector(`.modal__form-subtitle`);
+let saveProfileBtn = document.querySelector(`.form__save-btn`);
+let profile = {};
 
-image.addEventListener(`click`, function() {
-  popup.classList.add('popup_active');
-})
+function updateProfile() {
+  profile = {
+    name: title.textContent,
+    about: aboutMe.textContent,
+  };
+}
 
-modal_x.addEventListener(`click`, function() {
-  popup.classList.remove('popup_active');
-})
+function editProfile() {
+  updateProfile();
+  nameInput.value = profile.name;
+  jobInput.value = profile.about;
+}
+
+function closeProfile() {
+  popup.classList.remove("popup_active");
+}
+
+function saveProfile(evt) {
+  evt.preventDefault();
+  title.textContent = nameInput.value;
+  aboutMe.textContent = jobInput.value;
+  updateProfile();
+  closeProfile();
+}
+
+image.addEventListener(`click`, function () {
+  popup.classList.add("popup_active");
+  editProfile();
+});
+
+modal_x.addEventListener(`click`, closeProfile);
+saveProfileBtn.addEventListener(`click`, saveProfile);
